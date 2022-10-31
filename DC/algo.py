246ca562b3b4bd685d4f4094ad7852289159ebd8
Shobhit Kumar <kumar@shobhit.info>
2022-10-31 19:29:48 +0530

    Added a node definition for a site and its initialization
    It can now also initialize communication channels. Drops to
    a prompt to take user inputs
    
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- DC/algo.py
+++ DC/algo.py
@@ -1,7 +1,7 @@
 from time import sleep
 import channel
-
 import sys, getopt
+from node import Node
 
 def usage():
     print("python algo.py -i <site_id>")
@@ -9,42 +9,26 @@ def usage():
 
 def main (argv):
     site_id = None
-    site_port = None
-    dest_id = None
-    dest_port = None
     try:
-        opts, args = getopt.getopt(argv, "hs:p:d:i:")
+        opts, args = getopt.getopt(argv, "hi:")
     except:
         usage()
 
     for opt, arg in opts:
         if opt == '-h':
             usage()
-        elif opt == '-s':
-            site_id = int(arg)
-        elif opt == "-p":
-            site_port = int(arg)
-        elif opt == '-d':
-            dest_id = int(arg)
         elif opt == "-i":
-            dest_port = int(arg)
+            site_id = int(arg)
         else:
             usage()
 
-    if site_id is None or site_port is None or dest_id is None or dest_port is None:
+    if site_id is None:
         usage()
 
-    print(f"Site id: {site_id}")
-    print(f"Site port: {site_port}")
-    print(f"Dest Site id: {dest_id}")
-    print(f"Dest Site port: {dest_port}")
-
     # Driver to test
-    comms = channel.ChannelMgr(site_id, "127.0.0.1", site_port)
-    comms.start_receiver()
-    comms.open(dest_id, "127.0.0.1", dest_port)
-    sleep(5.0)
-    comms.send(dest_id, f"Hello from site: {site_id}")
+    localsite = Node(site_id)
+    localsite.initialize()
+    localsite.start()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ DC/node.py
@@ -0,0 +1,55 @@
+import sys
+from channel import ChannelMgr
+
+class Node:
+    def __init__(self, sid):
+        self.sites = {}
+        self.comms = None
+        self.sid = sid
+
+        # parse the config file to know about all the sites
+        # in the distributed system
+        try:
+            with open("system.cfg") as input:
+                for line in input:
+                    if line[0] == '#':
+                        # skip comments
+                        continue
+                    
+                    info = line.split(",")
+                    self.sites[int(info[0])] = {'ip': str(info[1].strip()), 'port': int(info[2])}
+
+                input.close()
+                print("Parsed system configuration.")
+        except IOError:
+            sys.exit("System config file could not be found/opened: system.cfg")
+        except:
+            sys.exit("File parsing error")
+
+    def dump_info(self):
+        print(str(self.sites))
+
+    def initialize(self):
+        try:
+            comms = ChannelMgr(self.sid, self.sites[self.sid]['ip'], self.sites[self.sid]['port'])
+
+            # start the thread to receive all inbound messages
+            comms.start_receiver()
+
+            # initialize outbound communication channel with all other sites
+            for key in self.sites:
+                if key == self.sid:
+                    continue
+
+                comms.open(key, self.sites[key]['ip'], self.sites[key]['port'])
+                print(f"Initialized params for channel: ({self.sid} -> {key})")
+            
+            print(f"Site ready: {self.sid}")
+        except:
+            sys.exit("Failed initializing site")
+
+    def start(self):
+        while 1:
+            cmd = input(f"(site:{self.sid})$ ")
+
+            # Process commands
