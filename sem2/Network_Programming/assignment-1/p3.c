c233d25a5dcc15a5aa8d5c85d797ad504be63a47
Shobhit Kumar <kumar@shobhit.info>
2023-04-20 05:06:07 +0000

    [NP] Assignment 1, Probelm 3 - Update problem statement in comments
    Signed-off-by: Shobhit Kumar <kumar@shobhit.info>

--- sem2/Network_Programming/assignment-1/p3.c
+++ sem2/Network_Programming/assignment-1/p3.c
@@ -1,4 +1,17 @@
 
+/**
+ * P3. Write a program which does the following. 
+ * 
+ * (a) Create N processes. N to be taken as an argument.
+ * (b) Every even process waits for a signal. Even by pid.
+ * (c) Every odd process sends SIGUSR1 signal to one of the even processes created prior to it. Even
+ *     process is chosen randomly.
+ * (d) When an even process receives more than M signals, it terminates itself after sending a SIGTERM
+ *     followed by SIGKILL to the last process which has sent SIGUSR1 to it. M is taken as an argument.
+ * (e) Everey process should print its pid, pid of the sending process, and the number of signals
+ *     received. Should print “Terminated Self” when exiting in case of even process. In case of odd
+ *     process, print “Terminated by <pid>”.
+*/
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -100,7 +113,6 @@ int main(int argc, char *argv[])
                     }
                 }
 
-                printf("event cnt: %d\n", even_cnt);
                 /* select random even process and send SIGUSR1 */
                 srand(time(0));
                 int num = rand() % even_cnt;
